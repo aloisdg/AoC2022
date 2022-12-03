c43b4aa1f6f0d2c270c437ce14c312608a380aaf
Aloïs de Gouvello <aloisdegouvello@live.fr>
2022-12-03 13:19:15 +0100

    Create one.cs

---
+++ day3/one.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+
+public class Program
+{
+	public static (string, string) Half(string s) => (s[..(s.Length / 2)], s[(s.Length / 2)..]);
+	public static int Prioritize(char c) => c - (char.IsLower(c) ? 'a' : ('A' - 26)) + 1;
+
+	public static void Main()
+	{
+		@"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"
+		.Split('\n')
+		.Select(Half)
+		.Select(t => t.Item1.Intersect(t.Item2).First())
+		.Sum(Prioritize)
+		.Dump();
+	}
+}
