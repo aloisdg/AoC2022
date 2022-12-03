edb83a90d6c78682c88ff244cdcf5c6f54c1a3ef
Aloïs de Gouvello <aloisdegouvello@live.fr>
2022-12-03 13:14:02 +0100

    Create one.cs

---
+++ day1/one.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+					
+public class Program
+{
+	public static void Main()
+	{
+		var s = @"1000
+2000
+3000
+4000
+5000
+6000
+7000
+8000
+9000
+10000";
+
+	var maxCalories = s
+		.Split("\n\n")
+		.Max(x => x.Split('\n').Sum(int.Parse));
+	Console.WriteLine(maxCalories);
+	}
+}
