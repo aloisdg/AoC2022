b6082a0c0f27013f50accab23631f8c4dffe08d4
Aloïs de Gouvello <aloisdegouvello@live.fr>
2022-12-04 13:49:46 +0100

    Create two.fs

---
+++ day4/two.fs
@@ -0,0 +1,15 @@
+let overlap a1 a2 b1 b2 = b2 >= a1 && b1 <= a2
+
+let count predicate = Seq.filter predicate >> Seq.length 
+    
+let s = """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"""
+
+s.Split '\n'
+|> Array.map (fun x -> x.Split [|','; '-'|] |> Array.map int)
+|> count(fun x -> overlap x.[0] x.[1] x.[2] x.[3])
+|> printfn "%i"
