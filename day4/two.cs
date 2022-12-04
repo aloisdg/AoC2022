ef1b1b6c940b6d617ebd05b054c43f9bba3fe7b6
Aloïs de Gouvello <aloisdegouvello@live.fr>
2022-12-04 11:22:56 +0100

    Create two.cs

---
+++ day4/two.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq;
+
+public class Program
+{
+	static bool Overlap(int a1, int a2, int b1, int b2) => b2 >= a1 && b1 <= a2;
+	
+	public static void Main()
+	{
+		@"2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"
+			.Split("\n")
+			.Select(x => x.Split(new[]{',', '-'}).Select(int.Parse).ToArray())
+			.Count(x => Overlap(x[0], x[1], x[2], x[3]))
+			.Dump();
+	}
+}
