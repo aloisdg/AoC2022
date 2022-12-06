c96d1e85cf0747142f8cbf7021ba774e832332ac
Aloïs de Gouvello <aloisdegouvello@live.fr>
2022-12-06 09:24:20 +0100

    Create one.py

---
+++ day6/one.py
@@ -0,0 +1,10 @@
+def find_marker_index(signal, marker):
+	for i in range(len(signal)):
+		if len(set(signal[i:i+marker])) == marker:
+			return i + marker
+
+	return -1;
+
+s = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+marker = 4
+print(find_marker_index(s, marker))
