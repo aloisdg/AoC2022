cf689a897de8ad1823545a0cdb348c7c9fc97bc0
Aloïs de Gouvello <aloisdegouvello@live.fr>
2022-12-06 08:48:25 +0100

    Create two.cs

---
+++ day6/two.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+					
+public class Program
+{
+	public static void Main()
+	{
+		var s = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+		var maker = 14;
+		FindMakerIndex(s, maker).Dump();
+	}
+	
+	public static int FindMakerIndex(string signal, int maker)
+	{
+		for (var i = 0; i < signal.Length; i++)
+		{
+			if (signal.Substring(i, maker).Distinct().Count() == maker)
+			{
+				return i + maker;
+			}
+		}
+		return -1;
+	}
+}
